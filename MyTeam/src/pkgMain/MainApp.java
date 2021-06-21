33db3f65df7eb5d0500cf7e8ad03da97c4b9fd16
schueler <schueler@ubuntu>
2021-06-21 15:39:12 +0200

    old player out

--- MyTeam/src/pkgMain/MainApp.java
+++ MyTeam/src/pkgMain/MainApp.java
@@ -7,7 +7,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
-		player = new Player(4, "Hinteregger", "Defense");
 		System.out.println("== " + player);
 		player = new Player (8, "Baumgartner", "Midlefield");
 		System.out.println("== " + player);
